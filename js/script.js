037bd491a266a942999353724b80dbff9b460864
sashutaMedvedev98 <62715686+sashutaMedvedev98@users.noreply.github.com>
2020-03-26 22:22:38 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+window.onload = function() {
+  let closePopUp = document.getElementsByClassName('catalog-popup__close');
+  let images = document.querySelectorAll('.slider-img');
+  let saleHeader = document.querySelectorAll('.sale-block__header');
+  let saleBody = document.querySelectorAll('.sale-block__body');
+  let buttonLeft = document.querySelector('.slider-button__left');
+  let buttonRight = document.querySelector('.slider-button__right');
+  let activeImage = "active";
+  let slider = new Slider(images, activeImage);
+  let listElement = document.querySelectorAll('.catalog-list__element');
+  let popUp = document.getElementsByClassName('catalog-popup');
+
+
+  listElement.forEach((item, i) => {
+    item.addEventListener('click', function(){
+      popUp[0].classList.add("activePopUp");
+    })
+  });
+
+  closePopUp[0].addEventListener('click', function(){
+    popUp[0].classList.remove("activePopUp");
+  })
+
+
+  function Slider(images, activeImage) {
+      this.images = images;
+      let i = 0;
+      this.prev = function () {
+          this.images[i].classList.remove(activeImage);
+          i--;
+
+          if (i < 0) {
+              i = this.images.length - 1;
+          }
+
+          this.images[i].classList.add(activeImage);
+      }
+
+      this.next = function () {
+          this.images[i].classList.remove(activeImage);
+          i++;
+
+          if (i >= this.images.length) {
+              i = 0;
+          }
+
+          this.images[i].classList.add(activeImage);
+      }
+  }
+
+  buttonLeft.addEventListener('click', function(){
+      slider.next();
+  });
+
+  buttonRight.addEventListener('click', function(){
+      slider.prev();
+  });
+
+  saleHeader.forEach((item, i) => {
+    item.addEventListener('click', function () {
+      if(saleBody[i].classList == 'sale-block__body show'){
+        saleBody[i].classList.remove('show');
+        saleHeader[i].classList.remove('saleHeader');
+      } else {
+        saleBody[i].classList.add('show');
+        saleHeader[i].classList.add('saleHeader');
+      }
+    })
+  });
+};
